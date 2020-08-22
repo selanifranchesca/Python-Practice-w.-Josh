b88431921477245deb64b068dcdab764c4e89ca3
Selani Thomas <selanifthomas@gmail.com>
2020-08-21 21:29:58 -0400

    we finished lesson 1

---
+++ lesson1.py
@@ -0,0 +1,10 @@
+name = input("Give me your name: ")
+print("Your name is " + name)
+age = int(input("Give me your age: "))
+num = 100 - age
+print("Your age is ")
+print(num)
+
+n =  int(input("Give me another number: "))
+for i in range(n): 
+    print(num) 
