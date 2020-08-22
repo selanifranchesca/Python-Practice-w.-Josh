02b66e588f155918d7451d03b3da50a6249cf325
Selani Thomas <selanifthomas@gmail.com>
2020-08-21 21:57:15 -0400

    we updated lesson5 with the help of Josh.

--- lesson5.py
+++ lesson5.py
@@ -3,23 +3,25 @@ b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
 # print([number for number in set(a) if number in set(b)])
 
-c = []
 
-for numberinb in b:
-    for numberina in a:
-        if numberinb == numberina and numberinb not in c:
+
+def compare (a,b):
+    c = []
+    for numberinb in b:
+        for numberina in a:
+            if numberinb == numberina and numberinb not in c:
                 c.append(numberinb)
 
-print(c) 
+    return c 
 
-# e = [1, 4, 7, 8, 12, 13, 15, 18, 23]
-# f = [1, 3, 7, 9, 13, 15, 16, 19, 23, 44]
-# g = []
+print(compare(a,b))
 
-# for number in e:
-#     if number in f:
-#         if number not in g:
-#             g.append(number)
+e = [1, 4, 7, 8, 12, 13, 15, 18, 23]
+f = [1, 3, 7, 9, 13, 15, 16, 19, 23, 44]
 
-# print(g)
+print(compare(e,f))
 
+def compareusingsets(a, b):
+    return a & b
+
+print(compareusingsets(set(a), set(b)))
