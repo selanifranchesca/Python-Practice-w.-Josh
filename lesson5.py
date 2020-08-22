4e23e0080fdacea9846bdfa5e3c5763fa8f51bcf
Selani Thomas <selanifthomas@gmail.com>
2020-08-21 21:52:43 -0400

    this is my fifth lesson.

---
+++ lesson5.py
@@ -0,0 +1,25 @@
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+# print([number for number in set(a) if number in set(b)])
+
+c = []
+
+for numberinb in b:
+    for numberina in a:
+        if numberinb == numberina and numberinb not in c:
+                c.append(numberinb)
+
+print(c) 
+
+# e = [1, 4, 7, 8, 12, 13, 15, 18, 23]
+# f = [1, 3, 7, 9, 13, 15, 16, 19, 23, 44]
+# g = []
+
+# for number in e:
+#     if number in f:
+#         if number not in g:
+#             g.append(number)
+
+# print(g)
+
