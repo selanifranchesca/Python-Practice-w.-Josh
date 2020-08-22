c9c16e6d137d32957e62ffe576157f8fbd6f884c
Selani Thomas <selanifthomas@gmail.com>
2020-08-21 21:58:14 -0400

    this is my sixth lesson.

---
+++ lesson6.py
@@ -0,0 +1,13 @@
+given_string = input("Give me a string: ")
+print(given_string)
+
+count = len(given_string) - 1
+for element in given_string:
+    if element == string:  
+        count += 1
+        if element == string:
+            count += -1
+    print("This string is a palindrome.")
+     else: 
+        print("This string is NOT a palindrome")
+
