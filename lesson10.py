cb38774921535a0b686f77e3df51e094075ec144
Selani Thomas <selanifthomas@gmail.com>
2020-09-01 22:12:04 -0400

    not sure if I totally get why thissolution works.

--- lesson10.py
+++ lesson10.py
@@ -1,6 +1,6 @@
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-c = []
+import random
 
-c = append.[i for i in a and x for x in b if i == x]
-print c
+a = random.sample(range(1,30), 12)
+b = random.sample(range(1,30), 16)
+result = [i for i in a if i in b]
+print (result)
