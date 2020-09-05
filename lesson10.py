470bb939eb3015b71c1219529eb2a86f62ad4b95
Selani Thomas <selanifthomas@gmail.com>
2020-09-04 21:47:43 -0400

    lesson 10 was modified to print both lists and use the sort function.

--- lesson10.py
+++ lesson10.py
@@ -3,4 +3,9 @@ import random
 a = random.sample(range(1,30), 12)
 b = random.sample(range(1,30), 16)
 result = [i for i in a if i in b]
-print (result)
+a.sort()
+print(a)
+b.sort()
+print(b)
+result.sort()
+print(result)
